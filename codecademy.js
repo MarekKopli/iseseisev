3ae8a0497e7d8e5d9312dc886d12ea4f9341c877
Marek Kopli <90028455+MarekKopli@users.noreply.github.com>
2021-11-11 22:39:58 +0200

    Update codecademy.js

--- codecademy.js
+++ codecademy.js
@@ -57,3 +57,11 @@ const plantNeedsWater = (day) => {
 //saab teha lihtsamalt
 const plantNeedsWater = day => day === 'Wednesday' ? true : false;
 -----------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+  
+const satellite = 'The Moon';
+const galaxy = 'The Milky Way';
+const stars  = 'North Star';
+const callMyNightSky = () => {
+  return 'Night Sky: ' + satellite + ', ' + stars + ', and ' + galaxy;
+}
+console.log(callMyNightSky())

871860c64d3f5c540eb925e48b72ea52088383c5
Marek Kopli <90028455+MarekKopli@users.noreply.github.com>
2021-11-30 22:21:29 +0200

    Update codecademy.js

--- codecademy.js
+++ codecademy.js
@@ -125,3 +125,18 @@ switch  (randomNumber){
 }
 console.log(`The eight ball answered: ${eightBall}`);
 //output Hello, iiii! am i going to die? The eight ball answered: It is decidedly so
+------------------------------------------------------------------------------------------
+let raceNumber = Math.floor(Math.random() * 1000);
+const registeredEarly = false;
+let runnerAge = 18;
+if(runnerAge>=18 && registeredEarly){
+  raceNumber+=1000;
+  console.log(`Hello runner ${raceNumber} Your race will start 9:30 am!` );
+}
+else if (runnerAge>=18 && !registeredEarly){
+  raceNumber+=1000;
+  console.log(`Hello runner ${raceNumber}  Your race will start 11.00 am!`);
+}
+else if (runnerAge< 18){
+  console.log(`Hello runner ${raceNumber} Your race will start 12:30 pm!`)
+}
